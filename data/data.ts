7e8eb7100bd2a5cdc0d632f0f1a7e1641c3cea4e
Ruthuvn <ruthuvnvisionex@gmail.com>
2025-10-25 19:58:46 +0530

    feat: add SVG logos for Tailwind, TypeScript, Webflow, and Vercel; include video files and Redux slices for fullpage, menu, and spline state management; configure Tailwind CSS and TypeScript settings

---
+++ data/data.ts
@@ -0,0 +1,12 @@
+export const links = {
+  home: "/",
+  about: "/about",
+  work: "/work",
+  contact: "https://t.me/vipulkumar_dev",
+  email: "mailto:ruthuparinika8@gmail.com",
+  resume: "/resume",
+  sourceCode: "https://github.com/vipulkumar-dev/portfolio",
+  github: "https://github.com/Ruthuvn",
+  linkedin: "https://www.linkedin.com/in/ruthu-parinika-a14a7b2b8?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  whatsapp: "https://api.whatsapp.com/send/?phone=918105055869&text=Hi+Ruthu%2C+I%27d+like+to+connect+regarding+a+project&type=phone_number&app_absent=0",
+};
